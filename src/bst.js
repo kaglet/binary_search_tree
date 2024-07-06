44462a6e999a8661529490f8b79e1a7e625e695b
Kago <kmotlhabane769@gmail.com>
2024-07-06 15:42:25 +0200

    Begin delete key implementation

--- src/bst.js
+++ src/bst.js
@@ -6,7 +6,7 @@ class BST {
         this.array = null;
     }
 
-    recursivelyBuild(array, start, end) {
+    static recursivelyBuild(array, start, end) {
         if (start > end) {
             return null;
         }
@@ -20,15 +20,15 @@ class BST {
         return root;
     }
 
-    buildTree(array) {
+    static buildTree(array) {
         let processedArr = array.sort((a, b) => a - b).filter((item, i, array) => {
             return item !== array[i - 1];
         });
 
-        return this.recursivelyBuild(processedArr, 0, processedArr.length);
+        return recursivelyBuild(processedArr, 0, processedArr.length);
     }
 
-    prettyPrint(node, prefix = "", isLeft = true) {
+    static prettyPrint(node, prefix = "", isLeft = true) {
         if (node === null) {
           return;
         }
@@ -39,6 +39,50 @@ class BST {
         if (node.getLeft() !== null) {
           this.prettyPrint(node.getLeft(), `${prefix}${isLeft ? "    " : "│   "}`, true);
         }
+    }
+
+    static insert(root, value) {
+        if (root === null) {
+            return new Node(value);
+        }
+
+        // Will either set the left or right child with a new node somewhere along the line
+        if (value < root.data) {
+            // Set property (same thing happens under hood whether implicitly or explicitly)
+            // So do not let use of a function confuse you in the recursion over an explicit assignment
+            root.setLeft(this.insert(root.getLeft(), value));
+        } else {
+            root.setRight(this.insert(root.getRight(), value));
+        }
+
+        // Return same node possibly with an altered reference in one of its children
+        return root;    
+    }
+
+    static delete(root, value) {
+        if (root === null) {
+            return new Node(value);
+        }
+
+        if (value < root.data) {
+            root.setLeft(this.delete(root.getLeft(), value));
+            return root;
+        } else if(value > root.data) {
+            root.setRight(this.delete(root.getRight(), value));
+            return root;
+        }
+
+        // If value vs root.data found this root is the node to be deleted, therefore consider different cases for deletion
+        // If you are deleting a node it depends what you are returning in it's place for the parent node setting its left and right children in the recursion
+        /* You can return either null not to be confused with the other null base case for when you return null which the left and right child of the parent was already set to.
+        This is due to the root being null so when returned into the parent it was always null originally anyways. */
+        /* If the root to delete has one child that is also easy. Simply return its child for its parent to pick up when the recursion is over and it is setting it's right or left child
+        as the path the recursion follows. */
+        /* If the root to delete has two children begin doing the following: 
+           1. Move to it's right subtree. 
+           2. Move to it's leftmost node in it's right subtree.
+           2. then recursively delete that element. 
+        */
     }
 }
 
